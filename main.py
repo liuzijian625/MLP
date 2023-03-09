69426be96b8dd62ff9d06862900c1c345cdfade1
liuzijian <1251052604@qq.com>
2023-03-09 19:06:46 +0800

    Changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import math
 
@@ -11,34 +10,46 @@ def sigmoid_gradient(x):
     return x * (1 - x)
 
 
+def linear(data, w, b):
+    result = data[0] * w[0] + data[1] * w[1] + b
+    return sigmoid(result)
+
+
 class OneLayerMLP:
     def __init__(self, train_data, test_data, learning_rate, momentum_constant):
         self.train_data = train_data
         self.test_data = test_data
         self.learning_rate = learning_rate
         self.momentum_constant = momentum_constant
-        self.w = np.random.random(2)
-        self.b = np.random.random(1)
+        self.w = np.random.random((2, 4))
+        self.b = np.random.random(2)
+        self.w_M = np.random.random(2)
+        self.b_M = np.random.random(1)
 
     def forward(self, data):
-        result = data[0] * self.w[0] + data[1] * self.w[1] + self.b
-        return sigmoid(result)
+        results = [data]
+        result = (linear(data, self.w[0], self.b[0]), linear(data, self.w[1], self.b[1]))
+        results.append(result)
+        result = linear(result, self.w_M, self.b_M)
+        results.append(result)
+        return results
 
-    def backward(self, e, result, data):
-        self.w = self.learning_rate * e * sigmoid_gradient(result) * data[0:1] + self.momentum_constant * self.w
-        self.b = self.learning_rate * e * sigmoid_gradient(result) + self.momentum_constant * self.b
+    def backward(self, e, results, data):
+        self.w_M = self.learning_rate * e * sigmoid_gradient(results[-1]) * data[
+                                                                            0:1] + self.momentum_constant * self.w_M
+        self.b_M = self.learning_rate * e * sigmoid_gradient(results[-1]) + self.momentum_constant * self.b_M
 
     def train(self):
         i = 1
         for data in self.train_data:
             print("第" + str(i) + "次训练")
-            result = self.forward(data)
-            e = data[2] - result
+            results = self.forward(data)
+            e = data[2] - results[-1]
             error = 0.5 * e * e
-            self.backward(e, result, data)
+            self.backward(e, results, data)
             print("error:" + str(error))
-            print(self.w)
-            print(self.b)
+            '''print(self.w)
+            print(self.b)'''
             i = i + 1
 
     def test(self):
@@ -46,13 +57,13 @@ class OneLayerMLP:
         total_num = 0
         for data in self.test_data:
             total_num = total_num + 1
-            result = self.forward(data)
-            if abs(result - data[2]) < 0.5:
+            results = self.forward(data)
+            if abs(results[-1] - data[2]) < 0.5:
                 right_num = right_num + 1
         print("正确率：" + str(100 * right_num / total_num) + "%")
 
     def decision_boundary(self):
-        a=1
+        a = 1
 
 
 if __name__ == '__main__':
