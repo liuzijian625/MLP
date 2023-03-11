3f7bb4a4ec57729b2928beaf4d75b5e9a1efb706
liuzijian <1251052604@qq.com>
2023-03-11 14:41:05 +0800

    Changes

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def sigmoid_gradient(x):
     return x * (1 - x)
 
 
-def linear(data, w, b):
-    result = data[0] * w[0] + data[1] * w[1] + b
+def linear(data, u, w, b):
+    result=b+(w*data).sum()
     return sigmoid(result)
 
 
@@ -27,39 +27,46 @@ class OneLayerMLP:
         self.test_data = test_data
         self.learning_rate = learning_rate
         self.momentum_constant = momentum_constant
-        self.w = np.zeros((32, 2))
-        self.b = np.zeros(32)
-        self.w_M = np.zeros(32)
-        self.b_M = np.zeros(1)
+        self.input_layer=2
+        self.hidden_layer=32
+        self.output_layer=1
+        self.train_num=1000
+        self.w = np.random.random((self.hidden_layer, self.input_layer))/10
+        self.b = np.zeros(self.hidden_layer)
+        self.w_M = np.random.random(self.hidden_layer)/10
+        self.b_M = np.zeros(self.output_layer)
+        self.losses=[]
+        self.accuracies=[]
+        self.train_order=[]
+        for i in range(self.train_num):
+            self.train_order.append(i+1)
 
     def forward(self, data):
         results = [data]
         result = []
-        for i in range(32):
-            result.append(linear(data, self.w[i], self.b[i]))
+        for i in range(self.hidden_layer):
+            result.append(linear(data[0:2],  self.w[i], self.b[i]))
         results.append(np.array(result))
-        result = linear(result, self.w_M, self.b_M)
+        result = linear(result,self.w_M, self.b_M)
         results.append(result)
         return results
 
     def backward(self, e, results):
         b_gradient = e * sigmoid_gradient(results[-1])
-        b_m = self.b_M + 0
+
+        w_m = self.w_M + 0
+
         self.w_M = self.learning_rate * e * sigmoid_gradient(results[-1]) * results[
             -2] + self.momentum_constant * self.w_M
         self.b_M = self.learning_rate * b_gradient + self.momentum_constant * self.b_M
-        for i in range(32):
-            self.w[i] = self.learning_rate * sigmoid_gradient(results[-2][i]) * results[
-                                                                                    -3][
-                                                                                0:1] * b_gradient * b_m + self.momentum_constant * \
-                        self.w[i]
-            self.b[i] = self.learning_rate * sigmoid_gradient(
-                results[-2][i]) * b_gradient * b_m + self.momentum_constant * self.b[i]
+        for i in range(self.hidden_layer):
+            self.w[i] = self.learning_rate * sigmoid_gradient(results[-2][i]) * results[-3][0:2] * b_gradient * (w_m[i]) + self.momentum_constant *self.w[i]
+            self.b[i] = self.learning_rate * sigmoid_gradient(results[-2][i]) * b_gradient *  (w_m[i])+ self.momentum_constant * self.b[i]
 
     def train(self):
-        self.acc_test = 0
         self.acc_train = 0
-        for j in tqdm(range(300)):
+        for j in tqdm(range(self.train_num)):
+            error_sum=0
             '''print("第" + str(j + 1) + "轮训练")'''
             i = 1
             for data in self.train_data:
@@ -67,10 +74,15 @@ class OneLayerMLP:
                 results = self.forward(data)
                 e = data[2] - results[-1]
                 error = 0.5 * e * e
+                error_sum=error_sum+error
                 self.backward(e, results)
                 '''print("error:" + str(error))'''
                 i = i + 1
             acc_now = self.test(self.train_data)
+            self.accuracies.append(acc_now)
+            self.losses.append(error_sum)
+            '''print(acc_now)
+            print(error_sum)'''
             if acc_now > self.acc_train:
                 self.j_train = j + 0
                 self.acc_train = acc_now + 0
@@ -78,18 +90,18 @@ class OneLayerMLP:
                 self.b_max_train = self.b + 0
                 self.w_M_max_train = self.w_M + 0
                 self.b_M_max_train = self.b_M + 0
-            acc_now = self.test(self.test_data)
+            '''acc_now = self.test(self.test_data)
             if acc_now > self.acc_test:
                 self.j_test = j + 0
                 self.acc_test = acc_now + 0
                 self.w_max_test = self.w + 0
                 self.b_max_test = self.b + 0
                 self.w_M_max_test = self.w_M + 0
-                self.b_M_max_test = self.b_M + 0
+                self.b_M_max_test = self.b_M + 0'''
         print("训练集最好结果在第" + str(self.j_train + 1) + "轮产生")
         print("准确率：" + str(self.acc_train) + "%")
-        print("测试集最好结果在第" + str(self.j_test + 1) + "轮产生")
-        print("准确率：" + str(self.acc_test) + "%")
+        '''print("测试集最好结果在第" + str(self.j_test + 1) + "轮产生")
+        print("准确率：" + str(self.acc_test) + "%")'''
 
     def test(self, datas):
         right_num = 0
@@ -133,17 +145,27 @@ class OneLayerMLP:
         plt.title("Decision Boundary")
         plt.show()
 
-    def test_prep(self):
+    '''def test_prep(self):
         self.b_M = self.b_M_max_test
         self.b = self.b_max_test
         self.w = self.w_max_test
-        self.w_M = self.w_M_max_test
+        self.w_M = self.w_M_max_test'''
 
     def train_prep(self):
         self.b_M = self.b_M_max_train
         self.b = self.b_max_train
         self.w = self.w_max_train
         self.w_M = self.w_M_max_train
+
+    def loss_change(self):
+        plt.clf()
+        plt.plot(self.train_order,self.losses)
+        plt.show()
+
+    def accuracy_change(self):
+        plt.clf()
+        plt.plot(self.train_order, self.accuracies)
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -155,4 +177,8 @@ if __name__ == '__main__':
     MLP.train()
     MLP.decision_boundary()
     '''MLP.test_prep()'''
-    '''MLP.test(test_data)'''
+    acc=MLP.test(test_data)
+    print("测试集准确率：" + str(acc) + "%")
+    MLP.loss_change()
+    MLP.accuracy_change()
+
